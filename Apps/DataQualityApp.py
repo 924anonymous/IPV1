4ec287a9ed936637cd679ad18abb4f03fcc2b202
varad.arthamwar <varad.arthamwar@globe.com.ph>
2023-05-28 12:51:07 +0530

    minor code changes

--- Apps/DataQualityApp.py
+++ Apps/DataQualityApp.py
@@ -18,62 +18,66 @@ def dataquality_app():
         st.error(e)
     else:
         try:
-            fdf = pd.DataFrame()
+            if len(log_df) > 0:
+                fdf = pd.DataFrame()
 
-            fdf['created_at'] = log_df.apply(lambda x: json.loads(x['errorneous_records'])['created_at'], axis=1)
-            fdf['updated_at'] = postgres_obj.execute_query("select current_timestamp::timestamp;").iloc[0][
-                'current_timestamp']
+                fdf['created_at'] = log_df.apply(lambda x: json.loads(x['errorneous_records'])['created_at'], axis=1)
+                fdf['updated_at'] = postgres_obj.execute_query("select current_timestamp::timestamp;").iloc[0][
+                    'current_timestamp']
 
-            log_df["errorneous_records"] = log_df.apply(
-                lambda row: Utility.key_operations(row["errorneous_records"], 'delete', '', ''), axis=1)
+                log_df["errorneous_records"] = log_df.apply(
+                    lambda row: Utility.key_operations(row["errorneous_records"], 'delete', '', ''), axis=1)
 
-            hide_table_row_index = """
-                                    <style>
-                                    thead tr th:first-child {display:none}
-                                    tbody th {display:none}
-                                    </style>
-                                    """
-            st.markdown(hide_table_row_index, unsafe_allow_html=True)
+                hide_table_row_index = """
+                                        <style>
+                                        thead tr th:first-child {display:none}
+                                        tbody th {display:none}
+                                        </style>
+                                        """
+                st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
-            all_rules_dict = {'All Rules': ['Null Check', 'Length Check', 'Boolean Check']}
-            all_rules_df = pd.DataFrame(all_rules_dict)
-            df_applied_rule = log_df.filter(items=['column_name', 'check_name']).drop_duplicates()
-            df_applied_rule['fix_value'] = ''
+                all_rules_dict = {'All Rules': ['Null Check', 'Length Check', 'Boolean Check']}
+                all_rules_df = pd.DataFrame(all_rules_dict)
+                df_applied_rule = log_df.filter(items=['column_name', 'check_name']).drop_duplicates()
+                df_applied_rule['fix_value'] = ''
 
-            with st.container():
-                left_col, right_col = st.columns(2)
-                with left_col:
-                    st.table(all_rules_df)
-                with right_col:
-                    with st.form("form_fix"):
-                        le_col, mid_col, ri_col = st.columns(3)
-                        for ind in df_applied_rule.index:
-                            with le_col:
-                                col_nm = st.text_input('Column Name', value=df_applied_rule['column_name'][ind])
-                            with mid_col:
-                                chk_nm = st.text_input('Check Name', value=df_applied_rule['check_name'][ind])
-                            with ri_col:
-                                f_val = st.text_input('Fix Value', value='', key=ind)
-                        submitted = st.form_submit_button("Submit")
-                        if submitted:
-                            for _ in range(len(df_applied_rule)):
-                                log_df["errorneous_records"] = log_df.apply(
-                                    lambda row: Utility.key_operations(row["errorneous_records"], 'add', col_nm, f_val),
-                                    axis=1)
+                with st.container():
+                    left_col, right_col = st.columns(2)
+                    with left_col:
+                        st.table(all_rules_df)
+                    with right_col:
+                        with st.form("form_fix"):
+                            le_col, mid_col, ri_col = st.columns(3)
+                            for ind in df_applied_rule.index:
+                                with le_col:
+                                    col_nm = st.text_input('Column Name', value=df_applied_rule['column_name'][ind])
+                                with mid_col:
+                                    chk_nm = st.text_input('Check Name', value=df_applied_rule['check_name'][ind])
+                                with ri_col:
+                                    f_val = st.text_input('Fix Value', value='', key=ind)
+                            submitted = st.form_submit_button("Submit")
+                            if submitted:
+                                for _ in range(len(df_applied_rule)):
+                                    log_df["errorneous_records"] = log_df.apply(
+                                        lambda row: Utility.key_operations(row["errorneous_records"], 'add', col_nm,
+                                                                           f_val),
+                                        axis=1)
 
-                            fdf['errorneous_records'] = log_df['errorneous_records']
-                            file_name = "fixed_errorneous_records_" + str(datetime.now().strftime("%d%m%Y%I%M%p"))
-                            fdf.to_parquet(file_name, engine='auto', compression='snappy')
-                            with open(file_name, "rb") as file:
-                                btn = st.download_button(
-                                    label="Download Parquet File",
-                                    data=file,
-                                    file_name=file_name
-                                )
-                            if btn:
-                                try:
-                                    os.remove(f'./{file_name}')
-                                except OSError as error:
-                                    pass
+                                fdf['errorneous_records'] = log_df['errorneous_records']
+                                file_name = "fixed_errorneous_records_" + str(datetime.now().strftime("%d%m%Y%I%M%p"))
+                                fdf.to_parquet(file_name, engine='auto', compression='snappy')
+                                with open(file_name, "rb") as file:
+                                    btn = st.download_button(
+                                        label="Download Parquet File",
+                                        data=file,
+                                        file_name=file_name
+                                    )
+                                if btn:
+                                    try:
+                                        os.remove(f'./{file_name}')
+                                    except OSError as error:
+                                        pass
+            else:
+                st.error('Erroneous Records Not Found 😁')
         except Exception as e:
             st.error(e)
