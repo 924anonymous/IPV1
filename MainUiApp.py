6908ab250ed4b9310719ce899d583fa05e656b7e
varad.arthamwar <varad.arthamwar@globe.com.ph>
2023-05-31 17:50:26 +0530

    Realtime dashboard added

--- MainUiApp.py
+++ MainUiApp.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from Apps import DashboardApp, UtilityApp, OperationsApp, DataQualityApp
+from Apps import DashboardApp, UtilityApp, OperationsApp, DataQualityApp, RealtimeKafkaDashboardApp
 
 with st.sidebar:
     selected_option = option_menu(
         "App Gallery",
-        ["Dashboard", "Accelerator", "Operations", "Data Quality"],
-        icons=["graph-up-arrow", "person-badge", "gear", "check-circle"],
+        ["Dashboard", "Accelerator", "Operations", "Data Quality", "RealTime Dashboard"],
+        icons=["graph-up-arrow", "person-badge", "gear", "check-circle", "graph-up-arrow"],
         styles={
             "container": {"padding": "5!important", "background-color": "#1b0c24"},
             "icon": {"color": "#e9f229", "font-size": "25px"},
@@ -23,3 +23,5 @@ if selected_option == "Operations":
     OperationsApp.operations_app()
 if selected_option == "Data Quality":
     DataQualityApp.dataquality_app()
+if selected_option == "RealTime Dashboard":
+    RealtimeKafkaDashboardApp.realtime_kafka_dashboard_app()
